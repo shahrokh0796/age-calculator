3bfc85945f427464a416c51f8890291b9e64c0ad
Said <shahrokh07.96@gmail.com>
2023-07-12 22:55:14 +0530

    age calculator js logic in progress

--- src/components/ageCalculation.js
+++ src/components/ageCalculation.js
@@ -1,19 +1,28 @@
 const formEl = document.querySelector("#form");
+const label = document.querySelectorAll("label");
 const dayEl = document.querySelector("#day");
 const monthEl = document.querySelector("#month");
 const yearEl = document.querySelector("#year");
-const errorEl = document.querySelector("small");
+const errorEl = document.querySelectorAll("small");
 const ageBtn = document.querySelector(".ageBtn");
+const displayAge = document.querySelectorAll("time");
 
-const isRequired = (inputLength) => inputLength.length == "" ? false: true;   
 
-function displayError(message, element) {
+console.log(label);
+
+const isRequired = (inputLength, amount) => inputLength.length < amount || inputLength.length > amount? false: true;   
+const isNumber = (input) => typeof input != 'number' ? false: true;
+
+
+function displayError(message, element, label) {
     element.textContent = message;
-    element.classList.add("error");
+    element.classList.add("err");
+    label.classList.add("err");
 }
-function removeError(element) {
+function removeError(element, label) {
     element.textContent = "";
-    element.classList.remove("error");
+    element.classList.remove("err");
+    label.classList.remove("err");
 }
 
 
@@ -21,9 +30,24 @@ function calculateAge(){
     let inputD = dayEl.value, inputM = monthEl.value,
     inputY = yearEl.value;
     const dOB = new Date(inputD,inputM,inputY).toLocaleDateString('en-GB');
-    if(!isRequired(inputD) || !isRequired(inputM) || !isRequired(inputY)) {
-        console.log("errororororororororororrrrr");
-    }
+    
+    if(!isRequired(inputD, 2)) {
+        displayError("Enter a valid date", errorEl[0], label[0]);
+    } else {
+        removeError(errorEl[0], label[0]);
+     }
+    
+    if(!isRequired(inputM, 2)) {
+        displayError("Enter a valid month", errorEl[1], label[1]);
+    } else {
+        removeError(errorEl[1], label[1]); 
+     }
+    
+     if(!isRequired(inputY, 4)) {
+        displayError("Enter a valid past", errorEl[2], label[2]);
+    } else {
+        removeError(errorEl[2], label[2]); 
+     }
 }
 
 ageBtn.addEventListener("click", calculateAge);
